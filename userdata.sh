34ce8436769cb07e7c5ec4d71e30d9e705e67d90
dhanalakshmilachu <dhanalakshmilachu894@gmail.com>
2025-12-19 13:44:16 +0530

    second commit

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 apt update -y
-
 apt install docker.io -y
 systemctl start docker
 systemctl enable docker
+
+# Clone repo
+cd /home/ubuntu
+git clone https://github.com/dhanalakshmilachu/tomcat-repo.git
+
+# Build Docker image
+docker build -t tomcat-app ./tomcat-repo
+
+# Run container mapping port 8080
+docker run -d -p 8080:8080 tomcat-app
 
