33ff355cc689bc669f08aeb9fb292c92473b5617
dhanalakshmilachu <dhanalakshmilachu894@gmail.com>
2025-12-19 20:58:14 +0530

    second commit

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Log everything
 exec > /var/log/userdata.log 2>&1
 
 echo "===== USERDATA STARTED ====="
@@ -9,35 +8,23 @@ echo "===== USERDATA STARTED ====="
 # Update system
 apt update -y
 
-# Install Java, wget, ca-certificates
+# Install Java & tools
 apt install -y openjdk-11-jdk wget ca-certificates tar
-
-# Verify Java
-java -version
 
 # Go to /opt
 cd /opt
 
 # Download Tomcat 9
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.113/bin/apache-tomcat-9.0.113.tar.gz
+wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.84/bin/apache-tomcat-9.0.84.tar.gz
 
-# Extract Tomcat
-tar -xvf apache-tomcat-9.0.113.tar.gz
+# Extract
+tar xzf apache-tomcat-9.0.84.tar.gz
+mv apache-tomcat-9.0.84 tomcat9
 
-# Rename directory
-mv apache-tomcat-9.0.113.tar.gz tomcat
-chown -R ubuntu:ubuntu /opt/tomcat
-# Make scripts executable
+# Permissions
 chmod +x /opt/tomcat9/bin/*.sh
 
-# Remove default apps
-rm -rf /opt/tomcat9/webapps/*
-
-# Download sample.war as ROOT.war
-wget https://tomcat.apache.org/tomcat-8.5-doc/appdev/sample/sample.war \
-     -O /opt/tomcat9/webapps/ROOT.war
-
-# Start Tomcat
+# START TOMCAT (do NOT delete webapps)
 /opt/tomcat9/bin/startup.sh
 
 echo "===== USERDATA COMPLETED ====="
