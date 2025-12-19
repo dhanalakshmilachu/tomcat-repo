061b6cadd5c4871aabc4346fac8bea16c7da9e21
dhanalakshmilachu <dhanalakshmilachu894@gmail.com>
2025-12-19 16:59:27 +0530

    second commit

--- userdata.sh
+++ userdata.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
-# Update OS
-yum update -y
-
-# Install Java
-amazon-linux-extras install java-openjdk11 -y
-
-# Install wget
-yum install wget -y
+# Update system
+apt update -y
+apt install openjdk-21-jdk wget -y
 
 # Download Tomcat
 cd /opt
 wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.84/bin/apache-tomcat-9.0.84.tar.gz
 tar xvf apache-tomcat-9.0.84.tar.gz
 mv apache-tomcat-9.0.84 tomcat9
+chmod +x /opt/tomcat9/bin/*.sh
 
 # Download sample.war as ROOT.war
 wget https://tomcat.apache.org/tomcat-8.5-doc/appdev/sample/sample.war -O /opt/tomcat9/webapps/ROOT.war
 
-# Set permissions
-chmod +x /opt/tomcat9/bin/*.sh
-
 # Start Tomcat
 /opt/tomcat9/bin/startup.sh
+
 
