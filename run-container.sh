4ee3b81b0a34261783861f21307cb957aab563bb
Dhanalakshmi A <dhanalakshmilachu894@gmail.com>
2025-12-20 13:07:07 +0530

    Delete run-container.sh

--- run-container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build Docker image
-docker build -t tomcat-sample .
-
-# Run container mapping host port 8080 to container 8080
-docker run -d -p 8080:8080 --name tomcat-sample-app tomcat-sample
-
